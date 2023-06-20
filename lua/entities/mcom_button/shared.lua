eec50a1f6c8518d2287ac8228ff3d2e2eed765cb
Lord Mirai (未来) <pilotcraft1001@yahoo.com>
2023-06-20 16:45:35 +0300

    restored these, as they seem to have been deleted

---
+++ lua/entities/mcom_button/shared.lua
@@ -0,0 +1,25 @@
+ENT.Type = "anim"
+ENT.Base = "mcom_peripheral_base" -- test if this works
+
+ENT.PrintName = "Button"
+
+ENT.Spawnable = true
+ENT.AdminOnly = true
+
+ENT.Author = "Lord Mirai　(未来)"
+ENT.Purpose = "A button input"
+ENT.Instructions = "Writes the value to a pin"
+
+
+function ENT:SetupDataTables()
+	self:NetworkVar("String", 0, "Serial")
+    self:NetworkVar("String", 1, "MAC")
+    self:NetworkVar("String", 2, "Type") -- peripheral type, freeform
+    self:NetworkVar("Entity", 0, "Parent") -- terminal or similar parent
+    self:NetworkVar("String", 3, "Port") -- parent port
+    self:NetworkVar("Bool", 0, "Active")
+end
+
+ENT.sounds = {
+    
+}
